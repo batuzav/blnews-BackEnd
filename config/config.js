38bb8042f538b4c55ac8c31c865be4272abfc084
Pedro Valdez <pvaldez@bodylogic.com.mx>
2020-10-05 17:36:35 -0500

    :hammer: F.- port error

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@
 ///////////////////
 // process.env.PORT = process.env.PORT || 443; //REGRESAR PUERTO A 443 //Descomentar cuando este en producci+on
 
-process.env.PORT = process.env.PORT || 4000;
+process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 process.env.DB =
   "mongodb+srv://user:ff258456@cluster0-qdmsk.mongodb.net/bodylogictest?retryWrites=true&w=majority";
