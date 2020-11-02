233cf5d9030f2f4221c79082758e7f401f1d289a
Pedro Valdez <pvaldez@bodylogic.com.mx>
2020-11-02 13:25:07 -0600

    [:heavy_plus_sign:] -.f dir public to download files

--- services/reportRayito.js
+++ services/reportRayito.js
@@ -55,7 +55,10 @@ app.get("/reportes", async (req, res) => {
                 // console.log(csv);
             
                 // write CSV to a file
-                fs.writeFileSync(path.join(__dirname, `/converted_report_${now}.csv`), csv);
+                fs.writeFileSync(path.join(__dirname, `/converted_report_${now}.csv`), csv, function (err) {
+                    if (err) throw err;
+                    console.log('File is created successfully.');
+                  });  
                 // res.download(`../public/converted_report_${now}.csv`, `converted_report_${now}.csv`);
             
             }).catch(err => console.log(err));
